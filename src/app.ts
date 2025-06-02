980b49752b2f7875172c50375276e4ee28f8d3b5
Mauricio Loya <mauloy15@hotmail.com>
2025-06-02 16:04:38 -0600

    Update testHelloFlow in app.ts: Changed response to include a PDF link and detailed information about seal pricing and ordering process.

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,11 @@ import {
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 
-const testHelloFlow = addKeyword<Provider, Database>(['testhello']).addAnswer(
-  'Hola sellos'
-)
+const testHelloFlow = addKeyword<Provider, Database>(['testhello'])
+  .addAnswer('https://lasermorelos.mx/assets/pdfs/cat2025v1.pdf')
+  .addAnswer(
+    'Hola buenas tardes! Gracias por escribirnos! Los sellos tienen un costo desde $250, el área del grabado es de 3 cm x 1cm por lo cual recomendamos máximo 3 renglones, el costo del envío es de $160 ya sea de uno o de varios, para registrar tu pedido es con el comprobante de pago, durante el proceso se te envía el diseño y cómo quedó antes de enviarlo! Te adjuntamos los modelos disponibles, Están de 4 a 8 días hábiles'
+  )
 
 const main = async () => {
   const adapterFlow = createFlow([testHelloFlow])
