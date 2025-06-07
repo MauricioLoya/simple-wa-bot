131f849346c06c00c2a5a1613722d0230cefc522
Mauricio Loya <mauloy15@hotmail.com>
2025-06-06 19:16:38 -0600

    Update testHelloFlow in app.ts: Enhanced responses with detailed information about seal pricing, ordering process, and delivery time.

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,21 @@ import {
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 
-const testHelloFlow = addKeyword<Provider, Database>(['testhello'])
+const testHelloFlow = addKeyword<Provider, Database>([
+  'informacion sellos',
+  'información sellos',
+  'informacion sellitos',
+  'información sellitos'
+])
   .addAnswer('https://lasermorelos.mx/assets/pdfs/cat2025v1.pdf')
   .addAnswer(
-    'Hola buenas tardes! Gracias por escribirnos! Los sellos tienen un costo desde $250, el área del grabado es de 3 cm x 1cm por lo cual recomendamos máximo 3 renglones, el costo del envío es de $160 ya sea de uno o de varios, para registrar tu pedido es con el comprobante de pago, durante el proceso se te envía el diseño y cómo quedó antes de enviarlo! Te adjuntamos los modelos disponibles, Están de 4 a 8 días hábiles'
+    '¡Hola! Gracias por escribirnos. Con gusto te mandamos el catálogo de los sellitos autoentintables que tenemos 😃 Contamos con envíos al interior de la república Mexicana en $160 ya sea de uno o de varios.'
   )
+  .addAnswer('Proceso de compra:')
+  .addAnswer(
+    'Para registrar tu pedido es con el comprobante de pago, durante el proceso se te envía el diseño y cómo quedó antes de enviarlo.'
+  )
+  .addAnswer('Te estaría llegando tu pedido en 1 semana aproximadamente. ✨')
 
 const main = async () => {
   const adapterFlow = createFlow([testHelloFlow])
